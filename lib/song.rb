a54a90e81b32e5f16c7d3770309f81a1899b37d1
Tommy Connor <tconnor98@aol.com>
2020-02-11 17:40:44 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,15 +1,4 @@
 class Song 
   attr_accessor :artist, :title
   
-  def initialize(name)
-    @artist = artist
-    @name = artist
-    @title = title
-  end
-  
-  def 
-    
-    
-    
-    
 end
